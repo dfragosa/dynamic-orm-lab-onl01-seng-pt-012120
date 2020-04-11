55e1b2312cf0e80942defe89e0b4b293483e6f60
Derric Fragosa <fderric@gmail.com>
2020-04-11 05:52:22 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -4,7 +4,15 @@ require 'active_support/inflector'
 class InteractiveRecord
   
   def self.table_name
-      
+sql = <<-SQL
+  CREATE TABLE IF NOT EXISTS students (
+  id INTEGER PRIMARY KEY, 
+  name TEXT, 
+  grade INTEGER
+  )
+SQL
+
+DB[:conn].execute(sql)
   end
   
 end
